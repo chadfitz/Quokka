491d4e667ab6011c1f300228384fdb6f38334feb
Noam Z <nzoam93@gmail.com>
2023-01-18 12:07:27 -0800

    cleaned up singlePinMap code

--- frontend/src/components/GoogleMap/SinglePinMap.js
+++ frontend/src/components/GoogleMap/SinglePinMap.js
@@ -1,23 +1,11 @@
-import { useState } from 'react';
-import {useSelector} from 'react-redux';
 import GoogleMapReact from 'google-map-react';
 import LocationMarker from './LocationMarker';
-import LocationInfoBox from './LocationInfoBox';
 import "./Map.css"
 
 const SinglePinMap = ({lat, lng}) => {
-    const [locationInfo, setLocationInfo] = useState(null);
-
-    // var marker = new google.maps.Marker({
-    //     position: myLatLng,
-    //     map: map,
-    //     title: 'Hello World!'
-    // });
-    const handleApiLoaded = (map, maps) => {
-        return (
-            {disableDefaultUI:true}
-        )
-    };
+    const mapOptions = {
+        disableDefaultUI: true
+    }
 
   return (
     <div className='map'>
@@ -26,25 +14,16 @@ const SinglePinMap = ({lat, lng}) => {
             defaultCenter={{lat: lat, lng: lng}}
             defaultZoom={5}
             yesIWantToUseGoogleMapApiInternals
-            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            options={mapOptions}
+            // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
         >
             <LocationMarker
                 lat={lat}
                 lng={lng}
-                onClick={()=> setLocationInfo({id: lat, title: lng})}
             />
         </GoogleMapReact>
-        {/* {locationInfo && <LocationInfoBox info={locationInfo}/>} */}
     </div>
   )
 }
-
-// Map.defaultProps = {
-//     center: {
-//         lat: 42.3265,
-//         lng: -122.8756
-//     },
-//     zoom: 6
-// }
 
 export default SinglePinMap
