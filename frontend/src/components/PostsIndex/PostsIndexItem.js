7180bca033374fcfeb68d4d190d0f4d92eda39aa
Noam Z <nzoam93@gmail.com>
2023-01-20 09:12:09 -0800

    added jump

--- frontend/src/components/PostsIndex/PostsIndexItem.js
+++ frontend/src/components/PostsIndex/PostsIndexItem.js
@@ -48,35 +48,41 @@ function PostsIndexItem ({ post }) {
                 {/* <img src={gmaps} alt="google maps location" id="post-google-map" /> */}
                 <SinglePinMap id="single-pin-map" lat={post.location?.coordinates[1]} lng={post.location?.coordinates[0]} key={post._id} />
             </div>
+
             <div className='post-item-middle'>
-                <h2 onClick={handleShow} className='post-item-subject'>{post.subject}</h2>
-                <h3 className='dear'>Dear {post.recipient.username},</h3>
-                {post.body && <Markup content={post.body} />}
-                <div className='post-item-photos'>
-                    {post.imageUrls ? <img id="post-item-photo" src={post.imageUrls[0]} alt=""/> :
-                "" }
+                <div className='post-index-date'>
+                    <h2 onClick={handleShow} className='post-item-subject'>{post.subject}</h2>
+                    <div>
+                        <img className="profile-image-item" src={post.writer.profileImageUrl} alt="profile" id="profile-image-item"/>
+                    </div>
                 </div>
-                <h3 className='signature'>From, <br/>{post.writer.username}</h3>
+                <div id='post-content'>
+                    <p className='dear'>Dear {post.recipient.username},</p>
+                    {post.body && <Markup content={post.body} />}
+                    <div className='post-item-photos'>
+                        {post.imageUrls ? <img id="post-item-photo" src={post.imageUrls[0]} alt=""/> :
+                    "" }
+                    </div>
+                    <p className='signature'>From, <br/>{post.writer.username}</p>
+                </div>
             </div>
-            <div className='post-index-date'>
-                <div>
-                    <img className="profile-image-item" src={post.writer.profileImageUrl} alt="profile" id="profile-image-item"/>
-                </div>
-                {sessionUser?._id === post.writer._id &&
-                <div className='post-index-date-lower'>
-                    <div className='post-index-icon' onClick={handleEdit}>< FiEdit3 /></div>
-                    <div className='post-index-icon' onClick={handleDelete}>< FiTrash2 /></div>
-                </div>
-                }
-            </div>
+
         </div>
         <div className='post-item-bottom-container'>
             <div className='post-item-bottom'>
                 {/* <h4>Post.reactions.count</h4> */}
                 {/* <button>React</button> */}
                 <h4>:) :( :D :_( -_-</h4>
-                <button>Reply</button>
-                <h4 id="time-ago"><time title={new Date(post.createdAt).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) }>{moment(post.createdAt).fromNow()}</time></h4>
+                <button id='reply-button-index-item'>Reply</button>
+                <div id='lower-right-corner-index-item'>
+                    {sessionUser?._id === post.writer._id &&
+                    <div className='post-index-date-lower'>
+                        <div className='post-index-icon' onClick={handleEdit}>< FiEdit3 /></div>
+                        <div className='post-index-icon' onClick={handleDelete}>< FiTrash2 /></div>
+                    </div>
+                    }
+                    <h4 id="time-ago"><time title={new Date(post.createdAt).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) }>{moment(post.createdAt).fromNow()}</time></h4>
+                </div>
             </div>
         </div>
     </div>
