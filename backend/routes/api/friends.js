b9dcd7108f1840247a53c1dc5c21a2dae8b45666
Will Bannister <bannister.will@gmail.com>
2023-01-22 17:39:06 -0800

    restructure friends backend

--- backend/routes/api/friends.js
+++ backend/routes/api/friends.js
@@ -11,6 +11,104 @@ const { requireUser } = require('../../config/passport');
 const { json } = require('express');
 const ObjectUtil = require("../../util/ObjectUtil")
 
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    let user = await User.findById(req.params.userId);;
+
+    const friendObjects = await Friend.find({
+      $or: [{ requester: user._id }, { recipient: user._id }]
+    });
+
+    const friendsFilter = friendObjects.map(friend => {
+      return [friend.requester.toString(), friend.recipient.toString()];
+    });
+
+    const friendIds = friendsFilter.flat().filter( el => {
+      return el.toString() !== user._id.toString();``
+    })
+
+    const friendsArray = await User.find({
+      '_id': { $in: friendIds }
+    });
+
+    const nonFriendsArray = await User.find({
+      '_id': { $nin: friendIds }
+    });
+
+    // var arr = [{key:"11", value:"1100"},{key:"22", value:"2200"}];
+    // const friendsObject = friendsArray.reduce(
+    //   (obj, item) => Object.assign(obj, { [item.key]: item.value }), {}
+    // );
+
+    console.log('friendsArray');
+    console.log(friendsArray);
+    console.log('nonFriendsArray');
+    console.log(nonFriendsArray);
+
+    const friendsObject = friendsArray.reduce((obj, item) => (obj[item._id] = item, obj) , {});
+    // const nonFriendsObject = nonFriendsArray.reduce((obj, item) => (obj[item._id] = item, obj) ,{})
+
+    // let friends = {};
+    // friendIds.forEach(async friendId => {
+    //   console.log('friendId');
+    //   console.log(friendId);
+    //   let friend = await User.findById(friendId.toString());
+    //   console.log('friend');
+    //   console.log(friend);
+    //   friends[friendId] = friend;
+    //   console.log('friends');
+    //   console.log(friends);
+    // });
+
+    console.log('friends');
+    console.log(friendsObject);
+    // const res = await friends.json();
+    // var object2 = friendObjects;
+    return res.json(
+      friendsObject
+      // nonFriends: nonFriendsObject
+    );
+  } catch(err) {
+    return res.status(404).json({ message: err.message });
+  }
+})
+
+router.post('/', upload.none(), async (req, res, next) => {
+  // const {requester, recipient, relation} = req
+  try {
+    let existingRelation = (
+      await Friend.findOne({
+        $and: [{ requester: req.body.requester }, { recipient: req.body.recipient }]
+      })
+      ||
+      await Friend.findOne({
+        $and: [{ recipient: req.body.requester }, { requester: req.body.recipient }]
+      })
+    );
+    // QUESTION: is this right? what should i be doing instead?
+    if (existingRelation) return {};
+    const newFriend = new Friend({
+      requester: req.body.requester,
+      recipient: req.body.recipient,
+      relation: req.body.relation
+    })
+
+    let entry = await newFriend.save();
+
+    const friendUser = await User.findById(req.body.recipient);
+    let friendObject = {[req.body.recipient]: friendUser};
+
+    return res.json(friendObject);
+  }
+  catch (err) {
+    err.statusCode = 404;
+    console.log('in err');
+    return res.status(404).json({ message: err.message });
+  }
+});
+
+
 router.delete('/:friendId', requireUser, async (req, res, next) => {
   const userId1 = req.user._id.toString();
   const userId2 = req.params.friendId;
@@ -37,56 +135,9 @@ router.delete('/:friendId', requireUser, async (req, res, next) => {
   }
 });
 
-router.get('/:userId', async (req, res, next) => {
-  try {
-    let user;
-    user = await User.findById(req.params.userId);
-    const friends = await Friend.find(
-      { $or: [{ requester: user._id }, { recipient: user._id }] }
-    );
-    var object1 = friends.map(friend => [friend.requester, friend.recipient]);
-    var object2 = object1.flat().filter( el => {
-      return el.toString() !== user._id.toString();``
-    })
-    return res.json(object2)
-  } catch(err) {
-    return res.status(404).json({ message: err.message });
-  }
-})
 
 
-router.post('/', upload.none(), async (req, res, next) => {
-  // const {requester, recipient, relation} = req
-  try {
-    let existingRelation = (
-      await Friend.findOne({
-        $and: [{ requester: req.body.requester }, { recipient: req.body.recipient }]
-      })
-      ||
-      await Friend.findOne({
-        $and: [{ recipient: req.body.requester }, { requester: req.body.recipient }]
-      })
-    );
-    // QUESTION: is this right? what shouuld i be doing instead?
-    if (existingRelation) return {};
-    const newFriend = new Friend({
-      requester: req.body.requester,
-      recipient: req.body.recipient,
-      relation: req.body.relation
-    })
-
-    let entry = await newFriend.save();
-
-    return res.json(newFriend);
-  }
-  catch (err) {
-    err.statusCode = 404;
-    console.log('in err');
-    return res.status(404).json({ message: err.message });
-  }
-});
-
-
+/*
 router.post('/acceptFriend', requireUser, async (req, res, next) => {
   // TODO: confirm that friendship does not already exist
   try {
@@ -113,7 +164,7 @@ router.post('/acceptFriend', requireUser, async (req, res, next) => {
     return next(error);
   }
 })
-
+*/
 
 // TODO: DELETE
   // let friend;
