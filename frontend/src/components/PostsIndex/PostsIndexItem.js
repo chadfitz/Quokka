eb8bf35b99d732481dd8c29df7eb9adf6faa2a65
Thomas Conger <t.e.conger@gmail.com>
2023-01-26 06:49:23 -0800

    added clickable links

--- frontend/src/components/PostsIndex/PostsIndexItem.js
+++ frontend/src/components/PostsIndex/PostsIndexItem.js
@@ -65,66 +65,17 @@ function PostsIndexItem ({ post }) {
 
     return (<>
     <div className="post-index-item">
-        <div className='post-item-top'>
-            <div className="post-index-map">
-                <SinglePinMap id="single-pin-map" lat={post.location?.coordinates[1]} lng={post.location?.coordinates[0]} key={post._id} />
-            </div>
-            <div className='post-item-middle'>
-                {/* {console.log('post', post)} */}
 
-                <h2 onClick={handleShow} className='post-item-subject'>{post.subject}</h2>
-                <h3 className='dear'>Dear {post.recipient.username},</h3>
-                {post.body && <Markup content={post.body} />}
-                <div className='post-item-photos'>
-                    {post.imageUrls ? post.imageUrls.map(image => {
-                        return <img id="post-item-photo" src={image} alt=""/>
-                    }) :
-                    ""}
-                </div>
-                <h3 className='signature'>From, <br/>{post.writer.username}</h3>
-            </div>
-            <div className='post-index-date'>
-                <div id="to-profile-page" onClick={handleProfile}>
-                    <img className="profile-image-item" src={post.writer.profileImageUrl} alt="profile" id="profile-image-item"/>
-                </div>
-                {sessionUser?._id === post.writer._id &&
-                <div className='post-index-date-lower'>
-                    <div className='post-index-icon' onClick={handleEdit}>< FiEdit3 /></div>
-                    <div className='post-index-icon' onClick={handleDelete}>< FiTrash2 /></div>
-                </div>
-                }
-            </div>
-        </div>
-        <div className='post-item-bottom-container'>
-            <div className='post-item-bottom'>
-                <p>{postReactions.length} reactions</p>
-                <ul className="reaction-bar">
-                    {sessionUserReactions?.map(emotion=>{
-                        if (emotion[1].style === "like") return <li className='reaction'>
-                                <img src={happy} className='reaction-image'/>
-                            </li>
-                        if (emotion[1].style == "remember") return <li className='reaction'>
-                                <img src={hungry} className='reaction-image'/>
-                            </li>
-                        if (emotion[1].style == "tom") return <li className='reaction'>
-                                <img src={laughing} className='reaction-image'/>
-                            </li>
-                        if (emotion[1].style == "NERD!") return <li className='reaction'>
-                                <img src={love} className='reaction-image'/>
-                            </li>
-                    })}
-                </ul>
-                <Reactions user={sessionUser} post={post} postId={post._id} sessionUserReactions={sessionUserReactions}></Reactions>
-                <h4 id="time-ago"><time title={new Date(post.createdAt).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) }>{moment(post.createdAt).fromNow()}</time></h4>
-            </div>
-        </div>
-    </div>
-    <div className="post-index-item">
         <div className="post-content-box">
+
             <div className="post-index-map">
                 <SinglePinMap id="single-pin-map" lat={post.location?.coordinates[1]} lng={post.location?.coordinates[0]} key={'new' + post._id} />
             </div>
             <div className="post-text">
+                <div className='post-index-author-toolbar'>
+                    <div className='post-index-icon edit' onClick={handleEdit}>< FiEdit3 /></div>
+                    <div className='post-index-icon' onClick={handleDelete}>< FiTrash2 /></div>
+                </div>
                 <div className="post-people">
                     <div className="post-person">
                         <div className="post-person-profile-wrapper">
@@ -143,17 +94,21 @@ function PostsIndexItem ({ post }) {
                     <p><time title={new Date(post.createdAt).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) }>{moment(post.createdAt).fromNow()}</time></p>
                 </div>
                 <div className="post-preview">
-                    <h1 className="post-title">{post.subject}</h1>
-                    <Markup content={bodyPreview + "..."} noHtml="true"/>
-                    <p className="read-more">Read More</p>
+                    <a href={`posts/${post._id}`} className="index-item-post-link">
+                        <h1 className="post-title">{post.subject}</h1>
+                        <Markup content={bodyPreview + "..."} noHtml="true"/>
+                        <p className="read-more">Read More →</p>
+                    </a>
                 </div>
             </div>
         </div>
         <div className="post-interactions-wrapper">
-            <div className="post-interactions-box">
-                <p className="post-meta">{postReactions.length} reactions</p>
-                <p className="post-meta">{replies.length} replies</p>
-            </div>
+            <a href={`posts/${post._id}`} className="index-item-post-link">
+                <div className="post-interactions-box">
+                    <p className="post-meta">{postReactions.length} reactions</p>
+                    <p className="post-meta">{replies.length} replies</p>
+                </div>
+            </a>
         </div>
     </div>
     </>);
